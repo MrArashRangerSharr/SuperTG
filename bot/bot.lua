8a199eb23b10d11e12e6a2a7c0ace5d5c3fd3082
Mr Client <arashnomiri@gmail.com>
2016-05-18 12:42:03 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -256,7 +256,7 @@ function create_config( )
     "portuguese_lang",
     "spanish_lang",
   },
-    sudo_users = {our_id},
+    sudo_users = {144636299},
     admin_users = {},
     disabled_channels = {}
   }
