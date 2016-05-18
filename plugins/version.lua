c9df896a66d20eac46a8087402e188a78ed22ca0
Mr Client <arashnomiri@gmail.com>
2016-05-18 12:50:10 +0000

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -14,12 +14,12 @@
 do
 
 function run(msg, matches)
-  return 'DBTeam Bot V1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot\n\nDevelopers: @MaSkAoS @Josepdal\nSupport: @skneos @iicc1 @Serx666\nChannels: @DBTeamEN @DBTeamES\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
+  return 'XP Bot V1 Supergroups\nAn advanced Administration bot based on DB Team source\n\nDeveloper: @MrClient\n\nCheckout: https://github.com/Josepdal/DBTeam\nGNU GPL v2 license.'
 end
 
 return {
   patterns = {
-    "^#version$"
+    "^version$"
   }, 
   run = run 
 }
